fc319c39da6761543b886520da77748dfcf2547d
gramz <gramz@LAB4-CAT4>
2024-04-29 08:36:57 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,16 @@
+﻿namespace EjemploHerencia
+{
+    public class Alumno : Persona
+    {
+        public string NoCarne { get; set; }
+        public List<int> Notas { get; set; }
+
+        public Alumno()
+        {
+            Notas = new List<int>();
+            NoCarne = string.Empty;
+        }
+
+
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EjemploHerencia
+{
+    public class Persona
+    {        
+
+     //   [Range(18, 80, ErrorMessage = "El número debe ser entre 18 y 80")]
+    //    public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es Obligatorio")]
+        public string Nombre { get; set; }
+        
+        [Required(ErrorMessage = "El apellido es Obligatorio")]
+        public string Apellido { get; set; }
+    
+        public string Direccion {  get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+                
+        //Constructor: Se ejecuta cada vez que se crea
+        //un objeto de esta clase
+        public Persona()
+        {
+            Nombre = "";
+            Apellido = "";
+            Direccion = "";
+        }
+
+        public int Edad()
+        {
+
+            TimeSpan edad = FechaNacimiento - DateTime.Now;
+
+            return Convert.ToInt16(edad.TotalDays / 365);
+        }
+    }
+}
